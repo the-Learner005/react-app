37fd248239b71a726d7c17f15b5cefb9e8fce81b
Ayesha Liaquat <ayesha.liaquat@jintech.com>
2019-11-28 12:04:46 +0500

    passing event handler from parent to child to update state of parent from child

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null
-    }
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     value: null
+  //   }
+  // }
     render() {
       return (
       <button className="square" 
-              onClick={() => {this.setState({value:'X'})}}
+              onClick={() => {this.props.onClick()}}
       >
-      {this.state.value}
+      {this.props.value}
       </button>
         
       );
@@ -21,10 +21,24 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value ={i} />;
+    constructor(props){
+      super(props);
+      this.state = {
+        squares : Array(9).fill(null)
+      }
     }
-  
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i]= 'X';
+      this.setState({squares:squares});
+    }
+    renderSquare(i) {
+      return <Square 
+                value ={this.state.squares[i]}
+                onClick= {()=>this.handleClick(i)} 
+              />;
+    }
+
     render() {
       const status = 'Next player: X';
   
